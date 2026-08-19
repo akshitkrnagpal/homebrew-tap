1521db209ae94d888fd579efa9a5c1320af466b3
akshitkrnagpal <akshitkrnagpal@gmail.com>
2026-08-19 04:18:48 +0400

    Update namescout to 0.2.0

--- Formula/namescout.rb
+++ Formula/namescout.rb
@@ -4,25 +4,25 @@
 class Namescout < Formula
   desc "Check name availability across domains, npm, and GitHub"
   homepage "https://github.com/akshitkrnagpal/namescout"
-  version "0.1.0"
+  version "0.2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/akshitkrnagpal/homebrew-tap/releases/download/namescout-v0.1.0/namescout_0.1.0_darwin_amd64.tar.gz"
-      sha256 "179db19c2ea17d43c95d5f9534ef302a1bd76a751864548a9232a568933a04b2"
+      url "https://github.com/akshitkrnagpal/homebrew-tap/releases/download/namescout-v0.2.0/namescout_0.2.0_darwin_amd64.tar.gz"
+      sha256 "3f6f795ac2260dd4a8f14a4d9425d399a3fdf6d346b7ebc1be64ecfc07c90556"
     elsif Hardware::CPU.arm?
-      url "https://github.com/akshitkrnagpal/homebrew-tap/releases/download/namescout-v0.1.0/namescout_0.1.0_darwin_arm64.tar.gz"
-      sha256 "c868fb583f304f4d40bf59ab29c718928278dd22df7d6c2196cc964b71a89af9"
+      url "https://github.com/akshitkrnagpal/homebrew-tap/releases/download/namescout-v0.2.0/namescout_0.2.0_darwin_arm64.tar.gz"
+      sha256 "8e8a46af1e1e1235a3279b8e6998f07084301f9970bae920680c398320cfdec8"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/akshitkrnagpal/homebrew-tap/releases/download/namescout-v0.1.0/namescout_0.1.0_linux_amd64.tar.gz"
-      sha256 "8b550d4d6b746639c54e51de6215a3928c8f833c6bcc3db194e4250696d739d4"
+      url "https://github.com/akshitkrnagpal/homebrew-tap/releases/download/namescout-v0.2.0/namescout_0.2.0_linux_amd64.tar.gz"
+      sha256 "10f7637eb9fc82cd91d5e9d90814c920982cb92033de24fc5e613aaa1684c176"
     elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/akshitkrnagpal/homebrew-tap/releases/download/namescout-v0.1.0/namescout_0.1.0_linux_arm64.tar.gz"
-      sha256 "afc487f052eb9bbe954cd4d90695e8647cecfce74cb2363c56f5ffce51f33830"
+      url "https://github.com/akshitkrnagpal/homebrew-tap/releases/download/namescout-v0.2.0/namescout_0.2.0_linux_arm64.tar.gz"
+      sha256 "5d5046bb71cb9598658fa69e8fef2a22c337526e3375a699bffdd6fd2f220cd5"
     end
   end
 
@@ -31,6 +31,6 @@ class Namescout < Formula
   end
 
   test do
-    assert_match "0.1.0", shell_output("#{bin}/namescout --version")
+    assert_match "0.2.0", shell_output("#{bin}/namescout --version")
   end
 end
